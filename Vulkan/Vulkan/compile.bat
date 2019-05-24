a1ce09c8b8b3ecf674f4ecc027c9083ba3ff3248
AkitsukiShintai <zxfwzq1995@gmail.com>
2019-05-24 20:43:54 +0800

    update

--- Vulkan/Vulkan/compile.bat
+++ Vulkan/Vulkan/compile.bat
@@ -1,3 +1,3 @@
-D:\VulkanSDK\1.1.92.1\Bin32\glslangValidator.exe -V shader.vert
-D:\VulkanSDK\1.1.92.1\Bin32\glslangValidator.exe -V shader.frag
+C:\VulkanSDK\1.1.106.0\Bin\glslangValidator.exe -V shader.vert
+C:\VulkanSDK\1.1.106.0\Bin\glslangValidator.exe -V shader.frag
 pause
